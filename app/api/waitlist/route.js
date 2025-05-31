36840c341c0c7948b73eda03cfd470f176675fb1
webbmaistro <webb.hammond93@gmail.com>
2025-05-31 13:19:10 -0400

    fixing some syntax

--- app/api/waitlist/route.js
+++ app/api/waitlist/route.js
@@ -11,7 +11,7 @@ export async function POST(request) {
   try {
     const { email } = await request.json()
     
-    const { data, error } = await supabase
+    const { error } = await supabase
       .from('waitlist')
       .insert([{ email }])
       .select()
