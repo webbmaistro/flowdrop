2b89c9edf6f921c4b3cd9d84d892cce63d614956
Webb <webb.hammond93@gmail.com>
2025-06-24 17:54:12 -0400

    Revert "hopefully we finally cooked those pesky api integrations"
    This reverts commit ce537645c33476ba256b19223a4147760757412b.

--- app/api/stripe/webhook/route.js
+++ app/api/stripe/webhook/route.js
@@ -8,9 +8,10 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: '2024-04-10',
 })
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const supabase = createClient(
+  'https://zocqlxonwsvhkamywijo.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpvY3FseG9ud3N2aGthbXl3aWpvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg2NTE2NDUsImV4cCI6MjA2NDIyNzY0NX0.sHKkSxqVa8WFvyaPj4z9WStGdDcR0tbaE6Ri1oasC9E'
+)
 
 async function readRawBody(readable) {
   const reader = readable.getReader()
--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -6,9 +6,10 @@ import GoogleSignIn from '../components/GoogleSignIn'
 import { createClient } from '@supabase/supabase-js'
 import { motion } from 'framer-motion'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-const supabase = createClient(supabaseUrl, supabaseAnonKey)
+const supabase = createClient(
+  'https://zocqlxonwsvhkamywijo.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpvY3FseG9ud3N2aGthbXl3aWpvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg2NTE2NDUsImV4cCI6MjA2NDIyNzY0NX0.sHKkSxqVa8WFvyaPj4z9WStGdDcR0tbaE6Ri1oasC9E'
+)
 
 export default function SignInPage() {
   const [email, setEmail] = useState('')
