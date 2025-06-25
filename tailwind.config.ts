93938220beaefc690f8f1558ee5118bda1561129
Webb <webb.hammond93@gmail.com>
2025-06-25 10:47:12 -0400

    Revert "styling import error fix"
    This reverts commit 18ed795ab4577d4d718db21598367b536bb86a46.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,12 +2,6 @@ import plugin from 'tailwindcss/plugin';
 
 /** @type {import('tailwindcss').Config} */
 const config = {
-  content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './lib/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
   theme: {
     extend: {
       colors: {
