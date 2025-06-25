18ed795ab4577d4d718db21598367b536bb86a46
Webb <webb.hammond93@gmail.com>
2025-06-25 08:43:50 -0400

    styling import error fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,12 @@ import plugin from 'tailwindcss/plugin';
 
 /** @type {import('tailwindcss').Config} */
 const config = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './lib/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
     extend: {
       colors: {
